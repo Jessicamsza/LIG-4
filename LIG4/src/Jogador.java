c9a7e1f4a839b0664f15b92cd9e12351ddeb4d32
marianant <101736929+marianant@users.noreply.github.com>
2023-06-25 14:35:37 -0300

    Update Jogador.java
    recebe a peça e verifica se é uma posição valida

--- LIG4/src/Jogador.java
+++ LIG4/src/Jogador.java
@@ -1,12 +1,19 @@
 public class Jogador {
-    private boolean Turn;
-    private char peca;
-
-    public Jogador(){
-
-    }
-    
-    public void setColumn() {
-        
-    }
-}
+	   private boolean turn;
+	   private char peca;
+	    
+	   public Jogador(boolean turn, char peca) {
+	       this.turn = turn;
+	       this.peca = peca;
+	    }
+	    
+	    public void setColumn(int posicao) {
+	        if (posicao < 1 || posicao > 7) {
+	            System.out.println("Posição inválida. A posição deve estar entre 1 e 7.");
+	            return;
+	        }
+	        
+	        System.out.println("Jogador " + (turn ? "A" : "B") + " jogou a peça " + peca + " na posição " + posicao + ".");
+	        
+	    }
+	}
