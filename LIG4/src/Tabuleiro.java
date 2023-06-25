0651400a581d30410abdf60aa2c6de355df86306
marianant <101736929+marianant@users.noreply.github.com>
2023-06-25 14:43:48 -0300

    Update Tabuleiro.java

--- LIG4/src/Tabuleiro.java
+++ LIG4/src/Tabuleiro.java
@@ -1,14 +1,25 @@
 public class Tabuleiro {
     private int[] matrizTabuleiro = new int[42]; 
     public Tabuleiro() {
-
+        Jogador jogador = new Jogador(true, 'X');
+        int posicao = 4; // Exemplo de posição de coluna
     }
 
     public void displayTabuleiro() {
         
     }
     public boolean validColumn() {
-        return true;
+        
+        boolean validColumn = isValidColumn(posicao);
+        
+        if (validColumn) {
+            jogador.setColumn(posicao);
+        } else {
+            System.out.println("Coluna inválida!");
+        }
     }
-
+    
+    public static boolean isValidColumn(int posicao) {
+        return posicao >= 1 && posicao <= 7;
+    }
 }
