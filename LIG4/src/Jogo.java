f1aa47d4b493c976906d5bd8f8e09a93149c9c3e
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-06-26 17:56:34 -0300

    Update Jogo.java

--- LIG4/src/Jogo.java
+++ LIG4/src/Jogo.java
@@ -1,25 +1,57 @@
-
+import java.util.Scanner;
 public class Jogo {
+    private Tabuleiro tabuleiro = new Tabuleiro();
+    private Jogador jog1 = new Jogador(true, 'X');
+    private Jogador jog2 = new Jogador(false, 'O');
+    private int escolhaJogador;
     
+
+    public Jogador jogadorDaRodada(){
+        if(jog1.getTurn()){
+                jog1.setTurn(false);
+                jog2.setTurn(true);
+                return jog1;
+            }else{
+                jog2.setTurn(false);
+                jog1.setTurn(true);
+                return jog2;
+            }
+    }
+
     public void jogar(){
-        Tabuleiro tabuleiro = new Tabuleiro();
-        Jogador jog1 = new Jogador(true, 'X');
+        Scanner scan = new Scanner(System.in);
         Jogada jogada = new Jogada();
 
+        System.out.println("Digite 9 para finzalizar; 0 para Resetar");
         tabuleiro.displayTabuleiro();
+        while(true){
+            Jogador jogador = jogadorDaRodada();
 
-        int escolhaJogador = jog1.seletcCol();
+            System.out.print("Selecione uma coluna para Jogar: ");
+            this.escolhaJogador = scan.nextInt();
+            
+            if(this.escolhaJogador == 9){
+                scan.close();
+                System.out.println("Fim do Jogo");
+                break;
+            }
+            if(this.escolhaJogador == 0){
+                tabuleiro.reset();
+                System.out.println("O jogo foi resetado!");
+                tabuleiro.displayTabuleiro();
+                continue;
+            }
 
-        if(tabuleiro.isValidCol(escolhaJogador)){
+            if(tabuleiro.isValidCol(escolhaJogador)){
+                jogada.setCol(escolhaJogador);
+                tabuleiro.posicionarPeca(jogada.getCol(), jogador.getPeca());
 
-            System.out.println("O jogador escolheu uma coluna Válida");
-            jogada.setCol(escolhaJogador);
-            tabuleiro.posicionarPeca(jogada.getCol(), jog1.getPeca());
+            } else {
+                System.out.println("Escolha inválida.");
+                continue;
+            }
 
-        } else {
-            System.out.println("Posição inválida. A posição deve estar entre 1 e 7.");
+            tabuleiro.displayTabuleiro();
         }
-
-        tabuleiro.displayTabuleiro();
     }
 }
