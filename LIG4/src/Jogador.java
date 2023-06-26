a780a7d3e7102d759b240a41e23b1977a34ca7da
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-06-25 23:40:42 -0300

    Update Jogador.java

--- LIG4/src/Jogador.java
+++ LIG4/src/Jogador.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 public class Jogador {
-	   private boolean turn;
-	   private char peca;
+	private boolean turn;
+	private char peca;
 	    
-	   public Jogador(boolean turn, char peca) {
-	       this.turn = turn;
-	       this.peca = peca;
-	    }
-	    
-	    public void setColumn(int posicao) {
-	        if (posicao < 1 || posicao > 7) {
-	            System.out.println("Posição inválida. A posição deve estar entre 1 e 7.");
-	            return;
-	        }
-	        
-	        System.out.println("Jogador " + (turn ? "A" : "B") + " jogou a peça " + peca + " na posição " + posicao + ".");
-	        
-	    }
+	public Jogador(boolean turn, char peca) {
+	    this.turn = turn;
+	    this.peca = peca;
 	}
+
+	public char getPeca() {
+		return peca;
+	}
+	
+	public int seletcCol(){
+		Scanner scan = new Scanner(System.in);
+		int escolhaJogador;
+
+		System.out.print("Selecione uma coluna para Jogar: ");
+        escolhaJogador = scan.nextInt();
+		scan.close();
+
+		return escolhaJogador;
+	}
+}
