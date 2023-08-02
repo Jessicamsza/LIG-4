713920c2fde0ec454baf642da83bffcd55447c14
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-08-02 20:28:37 -0300

    Update TabuleiroTurboMaluco.java

--- LIG4/src/game/tabuleiro/TabuleiroTurboMaluco.java
+++ LIG4/src/game/tabuleiro/TabuleiroTurboMaluco.java
@@ -11,26 +11,26 @@ public class TabuleiroTurboMaluco extends Tabuleiro{
 
     public void posicionarPeca(int col, char peca){
         int row = super.getLinhaDaJogada(col); 
-
-        if(row!=5 && vaiTrocarPecas()){
+        // cada peça vizinha (se existir) tem chance de virar a peca da rodada
+        if(row!=5 && vaiTrocarPecas()){//peça vizinha de baixo
             super.trocarPecas(row+1, col, peca);
         }
-        if(col!=0 && vaiTrocarPecas()){
+        if(col!=0 && vaiTrocarPecas()){//peça vizinha esqueda
             super.trocarPecas(row, col-1, peca);
         }
-        if(col!=6 && vaiTrocarPecas()){
+        if(col!=6 && vaiTrocarPecas()){//peça vizinha direita
             super.trocarPecas(row, col+1, peca);
         }
-        if(row!=0 && col!=0 && vaiTrocarPecas()){
+        if(row!=0 && col!=0 && vaiTrocarPecas()){//peça vizinha diagonal superior esquerda
             super.trocarPecas(row-1, col-1, peca);
         }
-        if(row!=0 && col!=6 && vaiTrocarPecas()){
+        if(row!=0 && col!=6 && vaiTrocarPecas()){//peça vizinha diagonal superior direita
             super.trocarPecas(row-1, col+1, peca);
         }
-        if(row!=5 && col!=0 && vaiTrocarPecas()){
+        if(row!=5 && col!=0 && vaiTrocarPecas()){//peça vizinha diagonal inferior esquerda
             super.trocarPecas(row+1, col-1, peca);
         }
-        if(row!=5 && col!=6 && vaiTrocarPecas()){
+        if(row!=5 && col!=6 && vaiTrocarPecas()){//peça vizinha diagonal infeiror direita
             super.trocarPecas(row+1, col+1, peca);
         }
 
